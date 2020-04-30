679586e62d3467f7e26933ca97408a5f2f8ece12
Xuzheng <linxz@garena.com>
2020-04-30 11:55:15 +0800

    Initial setup

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,17 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue';
+import Vuex from 'vuex';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
+  state: {
+    allDishes: []
+  },
+  mutations: {
+    SET_ALL_DISHES(state, allDishesData) {
+      state.allDishes = allDishesData;
+    }
+  },
   actions: {},
   modules: {}
 });
