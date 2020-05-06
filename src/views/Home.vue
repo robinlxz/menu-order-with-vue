d4d692ec05d5b306c5c8cd7cbac2e2a8d558ddf4
Xuzheng <linxz@garena.com>
2020-05-06 11:16:59 +0800

    UPD: homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <img alt="SingCafe" src="https://i.ibb.co/QY2b0cB/Sing-Cafe-board.jpg" />
+    <HelloWorld msg="Welcome to my Vue.js menu-order App" />
   </div>
 </template>
 
